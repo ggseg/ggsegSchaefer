4980cc124e27634cc1d5f95a05eb70fcf42fa1f6
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2020-03-28 18:48:26 +0100

    new mesh system

--- data-raw/palettes.R
+++ data-raw/palettes.R
@@ -17,3 +17,14 @@ brain_pals = list(
 )
 usethis::use_data(brain_pals, internal = TRUE, overwrite = TRUE)
 
+
+devtools::load_all("../../ggsegExtra/")
+schaefer17_3d <- ggsegExtra:::restruct_old_3datlas(schaefer17_3d)
+schaefer17_3d <- as_ggseg3d_atlas(schaefer17_3d)
+usethis::use_data(schaefer17_3d, internal = FALSE, overwrite = TRUE)
+
+
+schaefer7_3d <- ggsegExtra:::restruct_old_3datlas(schaefer7_3d)
+schaefer7_3d <- as_ggseg3d_atlas(schaefer7_3d)
+usethis::use_data(schaefer7_3d, internal = FALSE, overwrite = TRUE)
+
