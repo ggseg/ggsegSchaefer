abff1949bffd38291ffd9ceb3b45722d4a4822d5
monicathieu <mkt2131@columbia.edu>
2020-12-18 14:56:08 -0600

    make ggseg compatible pals
    for the 2d atlases that have been estimated so far

--- data-raw/palettes.R
+++ data-raw/palettes.R
@@ -1,102 +1,21 @@
-devtools::load_all("../../ggsegExtra/")
-devtools::load_all("../../ggseg/")
-devtools::load_all(".")
+require(tidyverse)
+require(magrittr)
+require(ggseg)
 
+# Eventuallly we can get all the pals this way
+# map2(rep(c(7, 17), times = 10),
+#      rep(seq(100, 1000, 100), each = 2),
+#      ~load(here::here("data", paste0("schaefer", .x, "_", .y, ".rda"))))
+load("data/schaefer7_100.rda")
+load("data/schaefer17_100.rda")
+load("data/schaefer7_200.rda")
+load("data/schaefer17_200.rda")
+load("data/schaefer7_400.rda")
 
-schaefer7_3d <- unnest(schaefer7_3d, ggseg_3d)
-schaefer7_3d <- mutate(schaefer7_3d,
-                       region = ifelse(grepl("wall|NA", region),
-                                       NA, region),
-                       colour = ifelse(is.na(region), NA, colour)
-)
-schaefer7_3d <- as_ggseg3d_atlas(schaefer7_3d)
+brain_pals <- list(schaefer7_100 = schaefer7_100$palette,
+                   schaefer17_100 = schaefer17_100$palette,
+                   schaefer7_200 = schaefer7_200$palette,
+                   schaefer17_200 = schaefer17_200$palette,
+                   schaefer7_400 = schaefer7_400$palette)
 
-
-schaefer17_3d <- unnest(schaefer17_3d, ggseg_3d)
-schaefer17_3d <- mutate(schaefer17_3d,
-                       region = ifelse(grepl("wall|NA", region),
-                                       NA, region),
-                       colour = ifelse(is.na(region), NA, colour)
-)
-schaefer17_3d <- as_ggseg3d_atlas(schaefer17_3d)
-
-# Make palette ----
-schaefer7_pal <- make_palette_ggseg(schaefer7_3d)
-schaefer17_pal <- make_palette_ggseg(schaefer17_3d)
-
-brain_pals <- c(schaefer7_pal, schaefer17_pal)
 usethis::use_data(brain_pals, internal = TRUE, overwrite = TRUE)
-devtools::load_all(".")
-
-# make atlas ----
-
-schaefer7 <- ggsegExtra::make_ggseg3d_2_ggseg(schaefer7_3d,
-                                               steps = 6:7,
-                                               tolerance = .1,
-                                               output_dir = "~/Desktop/test/")
-
-
-schaefer7$geometry <- NULL
-
-schaefer7 <- as_ggseg_atlas(schaefer7)
-
-ggseg(atlas=schaefer7, show.legend = FALSE,
-      colour = "black", position="stacked",
-      alpha=.6,
-      mapping = aes(fill=region)) +
-  scale_fill_brain("schaefer7", package = "ggsegSchaefer")
-
-usethis::use_data(schaefer7,
-                  internal = FALSE,
-                  overwrite = TRUE,
-                  compress="xz")
-
-
-schaefer17 <- ggsegExtra::make_ggseg3d_2_ggseg(schaefer17_3d,
-                                              steps = 6:7,
-                                              tolerance = .1,
-                                              output_dir = "~/Desktop/test/")
-
-schaefer17$geometry <- NULL
-schaefer17 <- as_ggseg_atlas(schaefer17)
-
-ggseg(atlas=schaefer17, show.legend = FALSE,
-      colour = "black", position="stacked",
-      alpha=.6,
-      mapping = aes(fill=region)) +
-  scale_fill_brain("schaefer17", package = "ggsegSchaefer")
-
-
-usethis::use_data(schaefer17,
-                  internal = FALSE,
-                  overwrite = TRUE,
-                  compress="xz")
-
-# # Make palette
-# k <- dplyr::slice(schaefer7_3d, 1) %>%
-#   unnest(ggseg_3d) %>%
-#   select(region, colour) %>%
-#   filter(!grepl("wall", region))
-#
-# j <- dplyr::slice(schaefer17_3d, 1) %>%
-#   unnest(ggseg_3d) %>%
-#   select(region, colour) %>%
-#   filter(!grepl("wall", region))
-#
-# brain_pals = list(
-#   schaefer7 = setNames(k$colour, k$region),
-#   schaefer17 = setNames(j$colour, j$region)
-# )
-# usethis::use_data(brain_pals, internal = TRUE, overwrite = TRUE)
-#
-#
-# devtools::load_all("../../ggsegExtra/")
-# schaefer17_3d <- ggsegExtra:::restruct_old_3datlas(schaefer17_3d)
-# schaefer17_3d <- as_ggseg3d_atlas(schaefer17_3d)
-# usethis::use_data(schaefer17_3d, internal = FALSE, overwrite = TRUE)
-#
-#
-# schaefer7_3d <- ggsegExtra:::restruct_old_3datlas(schaefer7_3d)
-# schaefer7_3d <- as_ggseg3d_atlas(schaefer7_3d)
-# usethis::use_data(schaefer7_3d, internal = FALSE, overwrite = TRUE)
-
